b60655e4a5dc4552b77ccdcb5d33e8554ccfe55f
Rohit Pavaskar <rohitpavaskar.19@gmail.com>
2018-07-02 14:04:00 +0530

    Added String Concatination for multiple lines

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Printf("Number Fromats Float: %f, Decimal: %d, Binary: %b, Hexadecimal: %X %x %o\n", 30.23, 30, 30, 30, 30, 30)
-	fmt.Printf("Characher: %c", 'A')
+	fmt.Printf("Number Formats Float: %f, Decimal: %d, Binary: %b, Hexadecimal: %X ,"+
+		"Hexadecimal: %x, Octal: %o\n",
+		30.23, 30, 30, 30, 30, 30)
+	fmt.Printf("Characher: %c, Charcter %c\n", 'A', 65)
 }
