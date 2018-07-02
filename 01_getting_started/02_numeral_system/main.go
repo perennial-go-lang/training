72276e6e160afbc9eb71905ee45054ba08ed367a
Ankit Kumbhar <kumbharankit23@gmail.com>
2018-07-02 13:56:19 +0530

    Remove semi-colons

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%d %b %o %#x\n", 42, 42, 42, 42)
-	fmt.Printf("%f %.3f\n", 42.0126232, 42.0126232)
-	fmt.Printf("%c", 65)
+	fmt.Printf("%d %o %x %X\n", 22, 32, 28, 28)
+	fmt.Printf("%d", 'a')
 }
--- 01_getting_started/02_numeral_system/numerical_prog.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Printf("%d %o %x %X\n", 22, 32, 28, 28);
-	fmt.Printf("%d", 'a');
-}
