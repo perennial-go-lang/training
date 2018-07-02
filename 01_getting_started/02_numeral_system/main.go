9ba608c11c561550744b044071fccff17c6b2b8b
Rohit Pavaskar <rohitpavaskar.19@gmail.com>
2018-07-02 15:08:15 +0530

    Formatted Output & Completed

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Number Formats Float: %f, Decimal: %d, Binary: %b, Hexadecimal: %X ,"+
-		"Hexadecimal: %x, Octal: %o\n",
-		30.23, 30, 30, 30, 30, 30)
-	fmt.Printf("Characher: %c, Charcter %c\n", 'A', 65)
+	fmt.Printf("Float: %f, Decimal: %d, Binary: %b, Octal: %o\n",
+		30.23, 30, 30, 30)
+	fmt.Printf("Hexadecimal(Capital): %X ,Hexadecimal(small): %x ,Hexadecimal(0x): %#x\n", 30, 30, 30)
+	fmt.Printf("Characher: %c, Character(Integer): %c\n", 'A', 65)
 }
