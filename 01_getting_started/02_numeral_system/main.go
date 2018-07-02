fa803e54d02bf0aa237033419753d2a2721bff5e
Rohit Pavaskar <rohitpavaskar.19@gmail.com>
2018-07-02 13:51:29 +0530

    Added Character formatting

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 
-	fmt.Printf("Number Fromats Float: %f, Decimal: %d, Binary: %b, Hexadecimal: %X %x %o", 30.23, 30, 30, 30, 30, 30)
+	fmt.Printf("Number Fromats Float: %f, Decimal: %d, Binary: %b, Hexadecimal: %X %x %o\n", 30.23, 30, 30, 30, 30, 30)
+	fmt.Printf("Characher: %c", 'A')
 }
