a48cde333745dd7f59572957282eaa52846af57c
soni.raju <soni.raju20@gmail.com>
2018-07-02 18:31:18 +0530

    Changes for showing Float numbers

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Integer->%d  Binary->%b  Octal->%o  Hexagon->%#X\n", 123, 123, 123, 123)
-	fmt.Printf("%f %.2f %3.3f\n", 123.456, 123.456, 123.456)
+	fmt.Printf("Integer-> %d  Binary-> %b  Octal-> %o  Hexagon-> %#X\n", 123, 123, 123, 123)
+	fmt.Printf("Float-> %f    Float with 2 decimals->%.2f\n", 123.45678, 123.45678)
 	fmt.Printf("Character Represantation of 58 -> %c\n", 65)
 }
