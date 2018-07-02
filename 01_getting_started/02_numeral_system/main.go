a1f4fc2a49fdb221396b2f84e844dc79a5d574e8
babulal_choudhary <babulal.choudhary@perennialsys.com>
2018-07-02 13:42:32 +0530

    #comment format output example

---
+++ 01_getting_started/02_numeral_system/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello\n")
+	fmt.Printf("Value : %v\n",10)
+	fmt.Printf("Binary : %b\n",10)
+	fmt.Printf("Decimal : %d\n",10)
+	fmt.Printf("Octal : %o\n",10)
+	fmt.Printf("Char : %c\n",'a')
+	fmt.Printf("Float : %f\n",20.323)
+	fmt.Printf("Float : %9.2f\n",20.36336)
+}
