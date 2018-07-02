cf0b85cd06eca9657f252c71e62fb47a90130467
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2018-07-02 13:32:44 +0530

    Improved output for Format Verb example

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%d %b %o %#x\n", 42, 42, 42, 42)
-	fmt.Printf("%f %.3f\n", 42.0126232, 42.0126232)
-	fmt.Printf("%c", 65)
+	//fmt.Printf("Decimal - %d \t Binary - %b \t Octal - %o \t Hexadecimal - %#X\n", 42, 42, 42, 42)
+	//fmt.Printf("Decimal - %d \t Binary - %b \t Octal - %o \t Hexadecimal - %#x\n", 42, 42, 42, 42)
+	//fmt.Printf("Decimal - %d \t Binary - %b \t Octal - %o \t Hexadecimal - %X\n", 42, 42, 42, 42)
+	fmt.Printf("Decimal - %d \t Binary - %b \t Octal - %o \t Hexadecimal - %x\n", 42, 42, 42, 42)
+	fmt.Printf("Float - %f \t Float accurate to 3 decimal places - %.3f\n", 42.0126232, 42.0126232)
+	fmt.Printf("Character (Integer = 65) - %c Integer (Character = 'a')  %d", 65, 'a')
 }
