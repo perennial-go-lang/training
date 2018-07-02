89f9a59daf9a297d66062f65ebcf018bb7cbbed3
soni.raju <soni.raju20@gmail.com>
2018-07-02 13:51:49 +0530

    Changes to check git email

--- 01_getting_started/02_numeral_system/main.go
+++ 01_getting_started/02_numeral_system/main.go
@@ -6,5 +6,5 @@ func main() {
 	fmt.Printf("%d\n", 32)
 	fmt.Printf("%d %b %o %#X\n", 32, 32, 32, 32)
 	fmt.Printf("%f %.2f %3.3f", 1.2365, 1.2365, 1.2365)
-	fmt.Printf("%c\n", 56)
+	fmt.Printf("%c\n", 55)
 }
