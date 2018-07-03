6c2d957227f1a6ae33114c6fa1a7ce5b9346860f
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-07-03 09:06:14 +0530

    Added Array Code

---
+++ 07_array_slice_and_maps/01_array/style3.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x [256]int
+	//var x [256]byte
+	//var x [256]string
+
+	fmt.Println("Length of Array", len(x))
+	fmt.Println("Value at 24th position", x[25])
+
+	for i := 0; i < 256; i++ {
+		x[i] = i
+	}
+
+	fmt.Println("Value \t Type \t Binary")
+	for i, v := range x {
+		fmt.Printf("%v \t\t %T \t %b\n", v, v, v)
+		if i > 50 {
+			break
+		}
+	}
+}
