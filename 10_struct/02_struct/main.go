9e8b9f097451a1baebd971f2e138a5e37a290149
soni.raju <soni.raju20@gmail.com>
2018-07-05 13:24:08 +0530

    Assigmnet of structs

---
+++ 10_struct/02_struct/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type person struct {
+	fName, lName, compName string
+}
+
+type employee struct {
+	person
+	empId int
+	fName string
+}
+
+func (p person) getDetails() string {
+	return "Name: " + p.fName + " " + p.lName + "\nComapny: " + p.compName
+}
+
+func (e employee) getDetails() string {
+	return "Emp Id:" + strconv.Itoa(e.empId) + "\nName: " + e.fName + " " + e.lName + "\nComapny: " + e.compName
+}
+
+func main() {
+	p1 := person{fName: "Soni", lName: "Raju", compName: "Perennial"}
+	fmt.Println(p1.getDetails())
+
+	fmt.Println("")
+
+	e1 := employee{person: person{fName: "SSS", lName: "Gandhi", compName: "Perennial System"}, empId: 120, fName: "Sonii"}
+	fmt.Println(e1.getDetails())
+}
